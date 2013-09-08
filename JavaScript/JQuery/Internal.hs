cffdc6f9aad24932d83c8c7f5a1387c48ef646de
Luite Stegeman <stegeman@gmail.com>
2013-09-08 18:39:34 +0200

    change safety spec for ajax import

--- JavaScript/JQuery/Internal.hs
+++ JavaScript/JQuery/Internal.hs
@@ -116,7 +116,7 @@ foreign import javascript unsafe "$2.siblings($1)"                    jq_sibling
 foreign import javascript unsafe "$2.slice($1)"                       jq_slice             :: Int                  -> JQuery -> IO JQuery
 foreign import javascript unsafe "$1.slice($1,$2)"                    jq_sliceFromTo       :: Int -> Int           -> JQuery -> IO JQuery
 
-foreign import javascript safe "jQuery.ajax($1,$2).always(function(d,ts,xhr) {\
+foreign import javascript interruptible "jQuery.ajax($1,$2).always(function(d,ts,xhr) {\
                                   if(typeof(d) === 'string') {\
                                     $c({ data: d, status: xhr.status });\
                                    } else {\
